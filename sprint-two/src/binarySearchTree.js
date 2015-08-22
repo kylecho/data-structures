3f66228ac42e2a0e58db18a683a179eedcd8cae2
kylecho <kylecho.work@gmail.com>
2015-08-21 21:42:32 -0700

    completed binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,59 @@
 var BinarySearchTree = function(value){
+  var tree = {};
+  tree.value = value;
+  tree.left = null;
+  tree.right = null;
+  _.extend(tree, searchTreeMethods);
+  return tree;
+};
+
+var searchTreeMethods = {};
+
+searchTreeMethods.insert = function(value){
+  if (this.value > value) {
+    if (this.left === null) { // base case
+      this.left = BinarySearchTree(value);
+    } else { // recursive case
+      this.left.insert(value);
+    }
+  } else if (this.value < value) {
+    if (this.right === null) {
+      this.right = BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  } else {
+    return false;
+  }
+};
+
+searchTreeMethods.contains = function(value){
+  // base case
+  if (this.value > value) {
+    if (this.left === null) {
+      return false;
+    } else {
+      return this.left.contains(value);
+    }
+  } else if (this.value < value) {
+    if (this.right === null) {
+      return false;
+    } else {
+      return this.right.contains(value);
+    }
+  } else {
+    return true;
+  }
+};
+
+searchTreeMethods.depthFirstLog = function(cb){
+  cb(this.value);
+  if (this.left !== null) {
+    this.left.depthFirstLog(cb); 
+  }
+  if (this.right !== null) {
+    this.right.depthFirstLog(cb); 
+  }
 };
 
 
