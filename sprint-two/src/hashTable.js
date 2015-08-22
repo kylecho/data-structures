093b143e8b32afc3e76b5d9ae039b45eda9e4e69
kylecho <kylecho.work@gmail.com>
2015-08-21 19:58:16 -0700

    Complete hashTable.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,41 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket;
+  var tuple = [k, v];
+  if (this._storage.get(i) === undefined) {
+    bucket = [tuple];
+  } else {
+    bucket = this._storage.get(i);
+    for (var j = 0; j < bucket.length; j++) {
+      if (bucket[j][0] === k) {
+        bucket[j][1] = v;
+      }
+    }
+    bucket.push(tuple);
+  }
+  this._storage.set(i, bucket);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var bucket = this._storage.get(i);
+  for (var j = 0; j < bucket.length; j++) {
+    if (bucket[j][0] === k) {
+      return bucket[j][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
+  for (var j = 0; j < bucket.length; j++) {
+    if (bucket[j][0] === k) {
+      bucket[j][1] = null;
+    }
+  }
+  this._storage.set(i, bucket);
 };
 
 
